7605559ff438d6d96b2a29dcf8532ea2ff99cea7
Aleksandr Gorovoi <alexander.gorovoy@vistar.su>
2015-10-30 14:29:26 +0300

    [http_server] Возможность использовать команды

--- src/srv_main.c
+++ src/srv_main.c
@@ -21,15 +21,30 @@ void* threadProcessor(void *arg){
         printf("%s", buffer);
 }
 
+void* socketProcessor(void *arg){
+	int socketDescr = *((int *) arg);
+	struct sockaddr_in serv_addr;
+        //Start listening
+        listen(socketDescr, MAX_CONNECTIONS);
+        while(1){
+                //Accept connection
+                int connfd = accept(socketDescr, (struct sockaddr*)NULL, NULL);
+                //Read info to bufer
+                threadProcessor(&connfd);
+                //Close connection
+                close(connfd);
+        }
+}
+
 int main(int argc, char* argv[]){
 	//iterators
 	int i = 0;
 	//default port
 	char port[4] = "8080";
 	for (i = 1; i < argc; i++){
-		if (strcmp(argv[i], "-p")){
-			if (i < argc){
-				strcpy(port, argv[i]);
+		if (!strcmp(argv[i], "-p")){
+			if (i + 1 < argc){
+				strcpy(port, argv[i+1]);
 				printf("Set port to %s", port);
 			} else {
 				printf("\nMissing argument for -p\n");
@@ -51,15 +66,20 @@ int main(int argc, char* argv[]){
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(atoi(port));
 	bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-	//Start listening
-	listen(sockfd, MAX_CONNECTIONS);
-	while(1){
-		//Accept connection
-		int connfd = accept(sockfd, (struct sockaddr*)NULL, NULL);
-		//Read info to bufer
-		threadProcessor(&connfd);
-		//Close connection
-		close(connfd);
+	//Socket processing
+	pthread_t socketProcessorThread;
+	pthread_create(&socketProcessorThread, NULL, &socketProcessor, &sockfd);
+	//Waiting for commands
+	while (1){
+		char command[100];
+		printf("httpserver $>");
+		scanf("%s", command);
+		if (!strcmp(command, "\\q")){
+			close(sockfd);
+			return 0;
+		} else {
+			printf("\nCommand not supported\n");
+		}
 	}
 	return 0;
 }
